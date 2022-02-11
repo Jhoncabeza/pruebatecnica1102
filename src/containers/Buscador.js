867c4aeb3b7784ae35201e033195c92929b20887
Jhon Cabeza <cabejhon7@gmail.com>
2022-02-11 12:58:17 -0500

    update

--- src/containers/Buscador.js
+++ src/containers/Buscador.js
@@ -3,7 +3,7 @@ import axios from "axios"
 import { useFormik } from 'formik';
 import { url } from '../apis/apis'
 import { db } from '../firebaseConfig';
-import { addDoc, collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
 
 
 export default function Buscador() {
